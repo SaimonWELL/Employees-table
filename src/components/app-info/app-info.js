05f26b63c26a1dcc3d343eefe11c97ea5ea26d3a
Saimon <saimon22011@gmail.com>
2024-03-08 11:46:37 +0400

    feat: created the layout for "app-info"

---
+++ src/components/app-info/app-info.js
@@ -0,0 +1,13 @@
+import './app-info.css';
+
+const AppInfo = () =>{
+    return(
+        <div className='app-info'>
+            <h1>Employee accounting at NoName Company</h1>
+            <h2>Total number of employees: </h2>
+            <h2>They'll get a bonus: </h2>
+        </div>
+    )
+}
+
+export default AppInfo;
--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,11 +1,15 @@
-
+import AppInfo from "../app-info/app-info";
 
 import './app.css';
 
 function App(){
     return(
         <div className='app'>
+            <AppInfo/>
 
+            <div className="search-panel">
+
+            </div>
         </div>
     )
 }
