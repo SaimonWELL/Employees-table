7d5ebf879bbe25db0eef457f5115b3fcac12c405
Saimon <saimon22011@gmail.com>
2024-03-12 09:50:53 +0400

    feat: added state  for 'cookies' button  and added state for favorite employee

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,31 +1,62 @@
+import {Component} from "react";
+
 import './employees-list-item.css';
 
-const EmployeesListItem = ({name,salary,increase}) =>{
-
-    let classNames = "list-group-item d-flex justify-content-between";
-    if(increase){
-        classNames+=' increase';
+class EmployeesListItem extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase:false,
+            favorite:false 
+        }
     }
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary+'$'}/>
-            <div className="d-flex justify-content-center align-items-center">
-                <button type="button"
-                        className="btn-cookie btn-sm">
-                    <i className="fas fa-cookie"></i>
-                </button>
+    onIncrease = () =>{
+        return this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
 
-                <button
-                    type="button"
-                    className="btn-trash btn-sm">
+    onFavorite = () =>{
+        this.setState(({favorite})=>({
+            favorite: !favorite
+        }))
+    }
+
+    render() {
+        const {name, salary} = this.props;
+        const {increase,favorite} = this.state
+
+        let classNames = "list-group-item d-flex justify-content-between";
+        if(favorite){
+            classNames+=' like'
+        }
+        if(increase){
+            classNames+=' increase';
+        }
+
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label"
+                      onClick={this.onFavorite}>{name}</span>
+                <input type="text" className="list-group-item-input" defaultValue={salary+'$'}/>
+                <div className="d-flex justify-content-center align-items-center">
+                    <button type="button"
+                            className="btn-cookie btn-sm"
+                            onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button
+                        type="button"
+                        className="btn-trash btn-sm">
                         <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
